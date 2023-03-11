9e56ddb5e1b5368363df1c40600773e8e2e797ae
Juan Francisco Vara <96703187+juanfranvs@users.noreply.github.com>
2023-03-11 18:30:37 +0100

    Actualización de Variables

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ client.on('connect', () => {
 })
 client.on('message', (topic, payload) => {
   const json = JSON.parse(payload.toString());
-  console.log(json.uplink_message.decoded_payload);
+  const data = (json.uplink_message.decoded_payload);
 
+  var object = {
+    'Nivel de Llenado Sensores' : data.bytes[0],
+    'Flag Latitud' : data.bytes[2],
+    'Valores Latitud' : [data.bytes[3], data.bytes[4], data.bytes[5]],
+    'Flag Longitud' : data.bytes[6],
+    'Valores Longitud' : [data.bytes[7], data.bytes[8], data.bytes[9]],  
+    'Bateria' : data.bytes[1],
+  }
+  console.log(object);
+  //Traza de prueba para comprobar que se asignan bien los datos
+  console.log(json.uplink_message.decoded_payload);
 })
