6ef3451ace791fed9368c7b4425b910670d65ace
Pablo Rubio Noguera <pabloru98@hotmail.com>
2023-03-13 17:39:02 +0100

    Modificado json

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mqtt = require('mqtt')
 const host = process.env.HOST
 const port = process.env.PORT
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+const lat_etsit = '40.4523805'
+const lng_etsit = '-3.7262121'
 
 const connectUrl = `mqtt://${host}:${port}`
 
@@ -11,7 +13,7 @@ const client = mqtt.connect(connectUrl, {
   clientId,
   clean: true,
   connectTimeout: 4000,
-  username: process.env.USERNAME,
+  username: 'end-device-lora1@ttn',
   password: process.env.PASSWORD,
   reconnectPeriod: 1000,
 })
@@ -27,16 +29,18 @@ client.on('message', (topic, payload) => {
   const json = JSON.parse(payload.toString());
   const data = (json.uplink_message.decoded_payload);
 
+  if (data.bytes[2] == 0){
+    latitud = lat_etsit;
+    longitud = lng_etsit;
+  }
   var object = {
-    'Nivel de Llenado Sensores' : data.bytes[0],
-    'Flag Latitud' : data.bytes[2],
-    'Valores Latitud' : [data.bytes[3], data.bytes[4], data.bytes[5], data.bytes[6]],
-    'Flag Longitud' : data.bytes[7],
-    'Valores Longitud' : [data.bytes[8], data.bytes[9], data.bytes[10], data.bytes[11]],  
+    'Nivel de Llenado Sensores' : data.bytes[0],    
+    'Latitud': latitud,
+    'Longitud' : longitud,      
     'Bateria' : data.bytes[1],
     'Fecha' : json.received_at
   }
   console.log(object);
   //Traza de prueba para comprobar que se asignan bien los datos
-  console.log(json.uplink_message.decoded_payload);
+  //console.log(json.uplink_message.decoded_payload);
 })
